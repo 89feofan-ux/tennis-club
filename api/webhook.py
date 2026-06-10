48a90158d1d2d6b10730b407d8a7ecb6f7331a22
Pavel <pavel@tennis.app>
2026-06-10 10:56:12 +0800

    Fix webhook syntax

--- api/webhook.py
+++ api/webhook.py
@@ -26,7 +26,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            length = int(self.headers.get(\'Content-Length\', 0))
+            length = int(self.headers.get('Content-Length', 0))
             body = json.loads(self.rfile.read(length).decode())
 
             msg = body.get("message", {})
@@ -59,7 +59,7 @@ class handler(BaseHTTPRequestHandler):
 
     def _json(self, code, obj):
         self.send_response(code)
-        self.send_header(\'Content-type\', \'application/json\')
-        self.send_header(\'Access-Control-Allow-Origin\', \'*\')
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(json.dumps(obj, ensure_ascii=False).encode())
